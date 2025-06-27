3c07eea765493fb6daab960f9fe850951fbf9c24
Dilanur Nigar Kaymak <dkaymak@k1m32s03.42istanbul.com.tr>
2025-06-27 17:59:40 +0300

    yeni

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaymak <dkaymak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 17:24:40 by dkaymak           #+#    #+#             */
+/*   Updated: 2025/06/27 17:47:39 by dkaymak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	dest_len = 0;
+	while (dest_len < size && dest[dest_len])
+		dest_len++;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (dest_len == size)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && dest_len + i + 1 < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	if (dest_len + i < size)
+		dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
---
+++ libft/ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaymak <dkaymak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 17:37:35 by dkaymak           #+#    #+#             */
+/*   Updated: 2025/06/27 17:48:21 by dkaymak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	last;
+
+	i = 0;
+	last = -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			last = i;
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	if (last == -1)
+		return (NULL);
+	return ((char *)&s[last]);
+}
